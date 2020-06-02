88baf3f5f999a755aa5d06e822b24e6495f3a3f0
floriansr <sueurfloriansf@gmail.com>
2020-06-02 08:57:07 +0200

    profile page added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:profile]
+
+  def profile
+  	@lastname = current_user.lastname.slice(0,1)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 	devise_for :users
+	get 'profile', to: 'users#profile'
   	root "static_pages#index"
 end
